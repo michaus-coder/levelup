ce2130cf06c2c0b12a8e7f81f5c6d3d6b5089e04
SastraH78 <sastrahimawan123@gmail.com>
2021-12-03 22:05:25 +0800

    hayo lihat apa

--- app/src/main/java/com/example/levelup/CreateEvent.kt
+++ app/src/main/java/com/example/levelup/CreateEvent.kt
@@ -1,11 +1,20 @@
 package com.example.levelup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CreateEvent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_event)
+
+//        val _buttons = findViewById<Button>(R.id.buttons)
+//        _buttons.setOnClickListener {
+//            val intentbs = Intent(this@MainActivity, CreateEvent::class.java)
+//            startActivity(intentbs)
+//        }
+
     }
 }
