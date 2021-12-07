2b8808a048a40865d59b319ffc06f1897e9f01bb
SastraH78 <sastrahimawan123@gmail.com>
2021-12-07 13:03:01 +0800

    hayo lihat apaa

--- app/src/main/java/com/example/levelup/CreateEvent.kt
+++ app/src/main/java/com/example/levelup/CreateEvent.kt
@@ -3,18 +3,35 @@ package com.example.levelup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.*
 
 class CreateEvent : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_event)
 
-//        val _buttons = findViewById<Button>(R.id.buttons)
-//        _buttons.setOnClickListener {
-//            val intentbs = Intent(this@MainActivity, CreateEvent::class.java)
-//            startActivity(intentbs)
-//        }
+        //Spinner
+        val spinner_data = arrayOf("1", "2", "3")
+        val _spinner_create_event = findViewById<Spinner>(R.id.spinner_create_event)
+        val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,spinner_data)
+        _spinner_create_event.adapter = arrayAdapter
+        _spinner_create_event.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+//                Toast.makeText(applicationContext, "Selected Cetegory is = " +spinner_data[position], Toast.LENGTH_SHORT).show()
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+        //End of spinner
+
 
     }
 }
