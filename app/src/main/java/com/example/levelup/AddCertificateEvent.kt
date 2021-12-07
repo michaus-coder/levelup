684f301de61b79ad5fd80a5cf28bdb4bd14e4fa9
bryanelmercahyadi <75104250+bryanelmercahyadi@users.noreply.github.com>
2021-12-07 10:06:07 +0800

    7-12-2021 - 10:04 AM

---
+++ app/src/main/java/com/example/levelup/AddCertificateEvent.kt
@@ -0,0 +1,11 @@
+package com.example.levelup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCertificateEvent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_certificate_event)
+    }
+}
