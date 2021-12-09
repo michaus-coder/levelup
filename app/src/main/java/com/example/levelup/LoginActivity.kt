88a796ef733735467a755509815a90e0680d387b
michaus-coder <c14190168@john.petra.ac.id>
2021-12-09 15:43:18 +0700

    9-12-2021 - 3:43 PM

--- app/src/main/java/com/example/levelup/LoginActivity.kt
+++ app/src/main/java/com/example/levelup/LoginActivity.kt
@@ -1,11 +1,28 @@
 package com.example.levelup
 
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val tvRegister = findViewById<TextView>(R.id.tvRegister)
+        tvRegister.setTextColor(Color.BLUE)
+
+        tvRegister.setOnClickListener{
+            tvRegister.setTextColor(Color.BLACK)
+            val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/levelup/MainActivity.kt
+++ app/src/main/java/com/example/levelup/MainActivity.kt
@@ -3,17 +3,49 @@ package com.example.levelup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    lateinit var navigationView : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        navigationView.setOnNavigationItemSelectedListener(this)
+        navigationView.selectedItemId = R.id.home
 
 //        val _buttons = findViewById<Button>(R.id.buttons)
 //        _buttons.setOnClickListener {
 //            val intentbs = Intent(this@MainActivity, DetailEvent::class.java)
 //            startActivity(intentbs)
 //        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.home -> {
+                true
+            }
+            R.id.profile -> {
+                val intent = Intent(this@MainActivity, ProfileActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.events -> {
+                val intent = Intent(this@MainActivity, EventsEvent::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.dashboard -> {
+                val intent = Intent(this@MainActivity, Dashboard::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> false
+        }
+        return true
     }
 }
