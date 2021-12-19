81fb35f33a11b421ebf98aefc3137ec8a7f68cd2
michaus-coder <c14190168@john.petra.ac.id>
2021-12-19 12:00:14 +0700

    19-12-2021 - 12:00 PM

--- app/src/main/java/com/example/levelup/ProfileActivity.kt
+++ app/src/main/java/com/example/levelup/ProfileActivity.kt
@@ -2,10 +2,13 @@ package com.example.levelup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        val floatingButtonAction = findViewById<FloatingActionButton>(R.id.floatingButtonActionEdit)
+        floatingButtonAction.alpha = 0.25f
     }
 }
